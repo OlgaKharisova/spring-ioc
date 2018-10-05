fc56ee550d9dca80eacc11f50db2300b86ae4d76
maks <maxim_86nva@mail.ru>
2018-10-05 12:47:16 +0300

    add singletonPattern

---
+++ src/ru/innopolis/stc13/patterns/creational/singleton/SingletonPattern.java
@@ -0,0 +1,18 @@
+package ru.innopolis.stc13.patterns.creational.singleton;
+
+public class SingletonPattern {
+
+    private static SingletonPattern ourInstance = null;
+
+    public static SingletonPattern getInstance() {
+        synchronized (SingletonPattern.class) {
+            if (ourInstance == null) {
+                ourInstance = new SingletonPattern();
+            }
+        }
+        return ourInstance;
+    }
+
+    private SingletonPattern() {
+    }
+}
