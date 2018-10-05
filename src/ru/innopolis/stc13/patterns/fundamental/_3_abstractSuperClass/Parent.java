986f8e57d95121226dfaed8c43aefe7dfc0efc81
ElviraDev <elvirates@gmail.com>
2018-10-05 12:52:34 +0300

    add abstractSuperClass pattern

---
+++ src/ru/innopolis/stc13/patterns/fundamental/_3_abstractSuperClass/Parent.java
@@ -0,0 +1,9 @@
+package ru.innopolis.stc13.patterns.fundamental._3_abstractSuperClass;
+
+public abstract class Parent {
+    public void figure() {
+        System.out.println("Parent figura");
+    }
+
+    public abstract void color(String color);
+}
---
+++ src/ru/innopolis/stc13/patterns/fundamental/_3_abstractSuperClass/Trangle.java
@@ -0,0 +1,8 @@
+package ru.innopolis.stc13.patterns.fundamental._3_abstractSuperClass;
+
+public class Trangle extends Parent{
+    @Override
+    public void color(String color) {
+        System.out.println(color + " Trangle");
+    }
+}
