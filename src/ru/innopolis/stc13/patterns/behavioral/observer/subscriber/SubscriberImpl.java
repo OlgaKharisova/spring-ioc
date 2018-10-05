0fa96293666d8fa890316d4ad9682388393623fc
sk0rn <hyperthrone@gmail.com>
2018-10-05 18:19:58 +1100

    corrections

--- src/ru/innopolis/stc13/patterns/behavioral/observer/subscriber/SubscriberImpl.java
+++ src/ru/innopolis/stc13/patterns/behavioral/observer/subscriber/SubscriberImpl.java
@@ -23,5 +23,18 @@ public class SubscriberImpl implements Subscriber {
         System.out.println(name + " go to " + event);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        SubscriberImpl that = (SubscriberImpl) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
